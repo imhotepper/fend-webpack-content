d12cfcf2f81bbf9879bbc1b9b9adff4427860219
dragosh <stefanescu.dragos@gmail.com>
2020-05-09 13:02:36 +0300

    webpack for dev and for prod added

---
+++ webpack.prod.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: "production",
+    entry: './src/client/index.js',
+    module: {
+        //loaders: they transform from one type to another
+        rules: [{
+
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    //plugins:
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
+}
